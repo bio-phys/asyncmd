b39af0ae60881aca1d0766f2c0c25bf7a577301d
hejung <hendrik.andre.jung@gmail.com>
2022-06-05 15:41:47 +0200

    WIP: started writing test for trajctory

--- tests/trajectory/test_trajectory.py
+++ tests/trajectory/test_trajectory.py
@@ -13,25 +13,101 @@
 # You should have received a copy of the GNU General Public License
 # along with asyncmd. If not, see <https://www.gnu.org/licenses/>.
 import pytest
+import numpy as np
+
+
+from asyncmd import Trajectory
 
 
 class Test_trajectory:
-    @pytest.mark.skip("TODO")
     @pytest.mark.parametrize(["traj_file", "struct_file", "truth"],
-                             [("../test_data/trajectory/ala_traj.trr",
-                               "../test_data/trajectory/ala.tpr",
+                             [("tests/test_data/trajectory/ala_traj.trr",
+                               "tests/test_data/trajectory/ala.tpr",
                                {"nstout": None,
+                                "first_step": 67740,
+                                "last_step": 68080,
+                                "dt": 0.04,
+                                "first_time": 135.48,
+                                "last_time": 136.16,
                                 }
                                ),
-                              ("../test_data/trajectory/ala_traj.xtc",
-                               "../test_data/trajectory/ala.tpr",
+                              ("tests/test_data/trajectory/ala_traj.xtc",
+                               "tests/test_data/trajectory/ala.tpr",
                                {"nstout": None,
+                                "first_step": 67740,
+                                "last_step": 68080,
+                                "dt": 0.04,
+                                "first_time": 135.48,
+                                "last_time": 136.16,
+                                }
+                               ),
+                              ("tests/test_data/trajectory/ala_traj.trr",
+                               "tests/test_data/trajectory/ala.gro",
+                               {"nstout": None,
+                                "first_step": 67740,
+                                "last_step": 68080,
+                                "dt": 0.04,
+                                "first_time": 135.48,
+                                "last_time": 136.16,
+                                }
+                               ),
+                              ("tests/test_data/trajectory/ala_traj.xtc",
+                               "tests/test_data/trajectory/ala.gro",
+                               {"nstout": None,
+                                "first_step": 67740,
+                                "last_step": 68080,
+                                "dt": 0.04,
+                                "first_time": 135.48,
+                                "last_time": 136.16,
                                 }
                                ),
                               ]
                              )
     def test_properties(self, traj_file, struct_file, truth):
-        pass
+        traj = Trajectory(trajectory_file=traj_file,
+                          structure_file=struct_file,
+                          )
+        for attr_name, truth_value in truth.items():
+            test_val = getattr(traj, attr_name)
+            if truth_value is None:
+                assert test_val is truth_value
+            elif isinstance(test_val, float):
+                # TODO: is atol=1e-5 really what we want?
+                # for our current tests it is fine I think
+                assert np.isclose(test_val, truth_value, atol=1e-5, rtol=0)
+            else:
+                assert test_val == truth_value
+
+    @pytest.mark.parametrize(["traj_file", "struct_file", "truth"],
+                             [("tests/test_data/trajectory/ala_traj.trr",
+                               "tests/test_data/trajectory/ala.tpr",
+                               {"__len__" : 18,
+                                }
+                               ),
+                              ("tests/test_data/trajectory/ala_traj.xtc",
+                               "tests/test_data/trajectory/ala.tpr",
+                               {"__len__": 18,
+                                }
+                               ),
+                              ("tests/test_data/trajectory/ala_traj.trr",
+                               "tests/test_data/trajectory/ala.gro",
+                               {"__len__": 18,
+                                }
+                               ),
+                              ("tests/test_data/trajectory/ala_traj.xtc",
+                               "tests/test_data/trajectory/ala.gro",
+                               {"__len__": 18,
+                                }
+                               ),
+                              ]
+                             )
+    def test_magic_methods(self, traj_file, struct_file, truth):
+        traj = Trajectory(trajectory_file=traj_file,
+                          structure_file=struct_file,
+                          )
+        for mm_name, truth_value in truth.items():
+            mm = getattr(traj, mm_name)
+            assert mm() == truth_value
 
     @pytest.mark.skip("TODO")
     def test_npz_cache(self):
