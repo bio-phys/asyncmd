e05ff39bdcbedfb898f331efbf2ffa090e957985
hejung <hendrik.andre.jung@gmail.com>
2025-06-24 19:10:13 +0200

    try again to get autodoc to work

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,10 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src/'))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                '../../src/')
+                                   )
+                )
 
 
 # -- Project information -----------------------------------------------------
